00562c13a63e52486d0b6d3c9ebbf6e9a3fb2278
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-08-03 00:57:28 +0530

    Login Page Modified

---
+++ Login App/MobApplication/app/src/main/java/com/example/mobapplication/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.mobapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    TextView welcomeLable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        welcomeLable = findViewById(R.id.welcomeLabel);
+
+        String email = getIntent().getStringExtra("email");
+        String result = "Welcome\n" + email;
+
+        welcomeLable.setText(result);
+    }
+}
