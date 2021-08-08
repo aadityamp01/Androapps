a35bc706db6dad821222fdfe59e911eb670e2f55
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-08-08 23:35:18 +0530

    View binding added

--- TODO List App/TODOList/app/src/main/java/com/todolist/MainActivity.kt
+++ TODO List App/TODOList/app/src/main/java/com/todolist/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.todolist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.todolist.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
