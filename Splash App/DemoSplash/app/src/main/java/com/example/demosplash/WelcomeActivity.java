131c2a8f70523c5b610e22559d75d8fef3ad4312
Gourav Khunger <gouravkhunger18@gmail.com>
2021-10-01 16:57:51 +0530

    Removed hard-coded strings and a bug from Splash App.

--- Splash App/DemoSplash/app/src/main/java/com/example/demosplash/WelcomeActivity.java
+++ Splash App/DemoSplash/app/src/main/java/com/example/demosplash/WelcomeActivity.java
@@ -12,6 +12,6 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Welcome");
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.welcome));
     }
 }
