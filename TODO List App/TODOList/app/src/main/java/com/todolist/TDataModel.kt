3582d41ac1660d23a3177d28df84f123f81031a1
Sudip Bhakat <sudipbhakat.04@gmail.com>
2021-10-02 21:17:10 +0530

    Added notes app

--- TODO List App/TODOList/app/src/main/java/com/todolist/TDataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.todolist
-
-// Creating a data model class
-class TDataModel(
-    val id: Int,
-    val Task: String,
-    val Description: String)
--- TextToSpeech App/TextToSpeech Apk/readme.txt
+++
@@ -1,5 +0,0 @@
-Download both the files
-
-app-debug.apk, output-metadata.json 
-
-in android phone, then install the apk.
