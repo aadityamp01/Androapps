1f0918cfc4a73e29f5f3b52c74861e364677cfa8
ElzaCS <anuejohn@gmail.com>
2021-10-09 20:39:20 +0530

    Improve evaluation of long expressions

--- Calculator App/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator App/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView data;
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     String oldNumber="";
     String op="+";
     boolean newOp= true;
+    Set<Character> oset = new HashSet<Character>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         data=findViewById(R.id.data);
         output=findViewById(R.id.output);
+        oset.add('+'); oset.add('-'); oset.add('*'); oset.add('/'); oset.add('%');
     }
 
     public void numberEvent(View view) {
-        if(newOp)
+        if(data.getText().toString().equals("0"))
             data.setText("");
         newOp=false;
         String number= data.getText().toString();
@@ -88,31 +94,82 @@ public class MainActivity extends AppCompatActivity {
             case R.id.percentage:
                 op="%";
                 break;
-
+            case R.id.brackets:
+                char last = oldNumber.charAt(oldNumber.length()-1);
+                if (last == '0' || oset.contains(last))
+                    op="(";
+                else
+                    op=")";
+                break;
         }
+        if(data.getText().toString().equals("0"))
+            data.setText(op);
+        else
+            data.setText(data.getText().toString()+op);
 
+    }
+    public static Double evaluate(String expression) {
+        char[] tokens = expression.toCharArray();
+        Stack<Double> values = new Stack<Double>();
+        Stack<Character> ops = new Stack<Character>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ')
+                continue;
+            if (tokens[i] >= '0' && tokens[i] <= '9'){
+                StringBuffer sbuf = new StringBuffer();
+                while (i < tokens.length && (tokens[i] == '.' || (tokens[i] >= '0' && tokens[i] <= '9')))
+                    sbuf.append(tokens[i++]);
+                values.push(Double.parseDouble(sbuf.toString()));
+                i--;
+            }
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+            else if (tokens[i] == ')') {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.push(tokens[i]);
+            }
+        }
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        return values.pop();
+    }
+
+    public static boolean hasPrecedence(char op1, char op2)  {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    public static Double applyOp(char op, Double b, Double a)  {
+        switch (op)            {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0)
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                return a / b;
+        }
+        return 0.0;
     }
 
     public void equalEvent(View view) {
         String newNumber=data.getText().toString();
         double result=0.0;
-        switch (op){
-            case "+":
-                result= Double.parseDouble(oldNumber)+Double.parseDouble(newNumber);
-                break;
-            case "-":
-                result= Double.parseDouble(oldNumber)-Double.parseDouble(newNumber);
-                break;
-            case "*":
-                result= Double.parseDouble(oldNumber)*Double.parseDouble(newNumber);
-                break;
-            case "/":
-                result= Double.parseDouble(oldNumber)/Double.parseDouble(newNumber);
-                break;
-
-        }
+        result = Double.parseDouble(String.valueOf(evaluate(newNumber)));
         output.setText(result+"");
-
     }
 
     public void clearEvent(View view) {
@@ -121,8 +178,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void percentEvent(View view) {
-        double no= Double.parseDouble(data.getText().toString())/100;
-        data.setText(no+"");
+        String curr = data.getText().toString();
+        int i=curr.length();
+        Double percent = 0.0;
+        int j=0;
+        while(i>0){
+            if (oset.contains(curr.charAt(i-1)))
+                break;
+            percent = Character.getNumericValue(curr.charAt(i-1))*Math.pow(10,j++) + percent;
+            i--;
+        }
+        percent = percent / 100;
+        curr = curr.substring(0,i) + percent.toString();
+        data.setText(curr);
         newOp=true;
     }
 }
